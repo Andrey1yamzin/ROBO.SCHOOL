337b77ca474a17ff18019a186bee320bf4ce60e4
rneirah <1yamzin98@mail.ru>
2023-08-16 23:04:21 +0700

    adaptive slider

--- js/slider.js
+++ js/slider.js
@@ -11,10 +11,9 @@ scrollbar: {
 },
 //отключение перетаскивания
 simulateTouch:false,
-//колличество для показа
-slidesPerView:3,
-//отступы между слайдами
-spaceBetween:40,
+
+
+
 //количество перелистыванных слайдов
 slidesPerGroup:1,
 //отцентрирование 1 слайда
@@ -24,6 +23,31 @@ slidesPerGroup:1,
 // loopedSlides:1,
 freeMode: false,
 // spped:100,
+breakpoints: {
+    320:{
+        slidesPerView:1,
+        spaceBetween:20,
+    },
+    480:{
+        slidesPerView:2,
+        spaceBetween:20,
+    },
+    
+    767:{
+        slidesPerView:4,
+        spaceBetween:40,
+    },
+    992:{
+        slidesPerView:4,
+        spaceBetween:30,
+    },
+    1920:{
+        //колличество для показа
+        //отступы между слайдами
+        slidesPerView:3,
+        spaceBetween:40,
+    }
+}
 }
 );
 
